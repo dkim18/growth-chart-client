356185739ea092b79ceaeed114946ba99957c7d0
Daniel Kim <tkkscf@gmail.com>
2020-10-14 02:28:57 -0500

    Update Constants.js

--- src/forms/Constants.js
+++ src/forms/Constants.js
@@ -1,2 +1,2 @@
-export const GROWTH_CHART_URL = 'http://ornlcodesample-env.eba-z9v52myc.us-east-2.elasticbeanstalk.com/chart/'
+export const GROWTH_CHART_URL = 'http://growthchart-env-1.eba-5u2btkp6.us-east-2.elasticbeanstalk.com/'
 export const FIRST_NAME = 'First Name'
